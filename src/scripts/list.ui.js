1a6e8ad1c33c9b7ccbb8602ceeb822aa9562fa11
Honorable con <honorablecon@gmail.com>
2022-01-01 16:04:09 +0000

    add modify to database

--- src/scripts/list.ui.js
+++ src/scripts/list.ui.js
@@ -1,3 +1,5 @@
+const {getFields} = require('./utils')
+
 function listUI(studentsDatabase) {
     let listStudentContenair = document.querySelector("#listStudentContenair");
     studentsDatabase.get().then( listStudents => {
@@ -22,12 +24,32 @@ function listUI(studentsDatabase) {
             })
             deleteBtn.addEventListener('click', e =>{
                 e.stopPropagation();
-                studentsDatabase.delete(student.id);
+                // studentsDatabase.delete(student.id);
                 element.remove()
             })
             editBtn.addEventListener('click', e=> {
                 e.stopPropagation();
-
+                let addForm = document.querySelector('#addForm')
+                let firstname = document.querySelector("#input-firstName");
+                let lastname = document.querySelector("#input-lastName");
+                let bio = document.querySelector("#bioTextArea");
+                let level = document.querySelector("#level-select").selectedOptions[0];
+                let frontendLevel = document.querySelector('#frontendLevel');
+                let backendLevel = document.querySelector('#backendLevel');
+                firstname.value = student.firstname
+                lastname.value = student.lastname
+                bio.value = student.bio
+                level.text = student.level
+                frontendLevel.value = student.progress.frontend
+                backendLevel.value = student.progress.backend
+                // let progress = {
+                //     frontend: parseInt(frontendLevel),
+                //     backend: parseInt(backendLevel)
+                // }
+                addForm.addEventListener('click', e => {
+                    e.preventDefault()
+                    studentsDatabase.modify(student.id, getFields())
+                })
             })
             listStudentContenair.appendChild(element);
         })
