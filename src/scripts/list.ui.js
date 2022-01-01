933a1af37cf365be6c71e96a36a1b4257cb47797
Honorable con <honorablecon@gmail.com>
2022-01-01 19:12:06 +0000

    modify card on list student

--- src/scripts/list.ui.js
+++ src/scripts/list.ui.js
@@ -7,7 +7,6 @@ function listUI(studentsDatabase) {
             let element = createElementFromTemplate("studentCard", student)
             let deleteBtn = element.lastElementChild.firstElementChild
             let editBtn = element.lastElementChild.lastElementChild
-            // element.setAttribute("data-id", student.id);
             element.dataset.dataId = student.id;
             element.addEventListener('click', e => {
                 listStudentContenair.classList.add('d-none');
@@ -16,6 +15,7 @@ function listUI(studentsDatabase) {
                 document.querySelector('.rowCard').appendChild(detail);
                 document.querySelector(".frontend").style.width = `${student.progress["frontend"]}%`;
                 document.querySelector(".backend").style.width = `${student.progress["backend"]}%`;
+
                 cancelBtn.addEventListener('click', e => {
                     e.preventDefault()
                     detail.classList.add('d-none');
@@ -24,7 +24,7 @@ function listUI(studentsDatabase) {
             })
             deleteBtn.addEventListener('click', e =>{
                 e.stopPropagation();
-                // studentsDatabase.delete(student.id);
+                studentsDatabase.delete(student.id);
                 element.remove()
             })
             editBtn.addEventListener('click', e=> {
@@ -42,13 +42,19 @@ function listUI(studentsDatabase) {
                 level.text = student.level
                 frontendLevel.value = student.progress.frontend
                 backendLevel.value = student.progress.backend
-                // let progress = {
-                //     frontend: parseInt(frontendLevel),
-                //     backend: parseInt(backendLevel)
-                // }
+
                 addForm.addEventListener('click', e => {
-                    e.preventDefault()
-                    studentsDatabase.modify(student.id, getFields())
+                    e.preventDefault();
+                    let fields = getFields();
+                    studentsDatabase.modify(student.id, fields);
+                    let infoStudent = element.children[1];
+                    let name = infoStudent.firstElementChild
+                    let level = infoStudent.children[1]
+                    let bio = infoStudent.lastElementChild
+
+                    name.innerHTML = `${fields.firstname} ${fields.lastname}`
+                    level.innerHTML = fields.level
+                    bio.innerHTML = fields.bio
                 })
             })
             listStudentContenair.appendChild(element);
